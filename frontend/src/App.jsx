6f31d89f53f6c788da3d180e0698f908a0cfb663
Saurav Kosti <kosti.saurav@gmail.com>
2025-04-12 13:32:46 +0530

    feat: initialize frontend with Vite and React + Chkra UI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Provider } from "@/components/ui/provider"
+
+function App() {
+
+  return (
+    <Provider>
+      <h1>Welcome to the App</h1>
+    </Provider>
+  )
+}
+
+export default App
